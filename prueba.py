6e5624984ae2aaa8f999e91707f9622857f28f43
Marco <ma.diaz@ccbantigua.edu.gt>
2023-08-23 11:34:07 -0600

    versión 3 comandos

--- prueba.py
+++ prueba.py
@@ -1,2 +1,10 @@
 print("tengo sueño, sanji es un genio incomprendido ")
 print("Ya no tengo sueño")
+#git clone(clonar un repositorio)
+#git add . {todos} (para hacer un stage changes)
+#git commit -m "nombre" (sello para otra versión local)
+""" Unsave changes, stage changes committed changes
+    git push origin main (subir cambios)
+    git pull origin main (bajar los cambios)
+"""
+
